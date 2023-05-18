597cb0c22dc768495fb84807c42f9042d58f016f
fr33m0nk <prashantsinha@protonmail.com>
2023-05-18 10:34:36 +0530

    [fr33m0nk]: Adds documentation for `fr33m0nk.alpakka-kafka.producer`

--- src/fr33m0nk/alpakka_kafka/producer.clj
+++ src/fr33m0nk/alpakka_kafka/producer.clj
@@ -1,7 +1,7 @@
 (ns fr33m0nk.alpakka-kafka.producer
   (:require [clojure.string :as str])
   (:import (akka.actor ActorSystem)
-           (akka.kafka CommitterSettings ConsumerMessage$CommittableMessage ProducerMessage ProducerMessage$Envelope ProducerSettings ProducerMessage$Result ProducerMessage$Results)
+           (akka.kafka CommitterSettings ProducerMessage ProducerMessage$Envelope ProducerSettings ProducerMessage$Result ProducerMessage$Results)
            (akka.kafka.javadsl Producer SendProducer)
            (java.util Collection Map)
            (java.util.concurrent CompletionStage)
@@ -9,8 +9,10 @@
            (org.apache.kafka.common.serialization Serializer)))
 
 (defn producer-settings
-  "Expects producer properties to be supplied with kebab-case-keyword keys
-  Full config list can be found in org.apache.kafka.clients.producer.ProducerConfig"
+  "Settings for producers. See akka.kafka.producer section in reference.conf
+  https://doc.akka.io/api/alpakka-kafka/4.0.2/akka/kafka/ProducerSettings.html
+  - Expects producer-properties to be supplied with kebab-case-keyword keys
+    Full config list can be found in org.apache.kafka.clients.producer.ProducerConfig"
   ^ProducerSettings
   [^ActorSystem actor-system {:keys [key-serializer value-serializer bootstrap-servers] :as producer-properties}]
   (let [producer-config (-> producer-properties
@@ -21,8 +23,9 @@
         (.withProperties ^Map producer-config))))
 
 (defprotocol IProducerMessage
-  (producer-message-passthrough [this])
-  (producer-message-with-passthrough [this passthrough]))
+  "PassThroughMessage does not publish anything, and continues in the stream as PassThroughResult"
+  (producer-message-passthrough [this] "https://doc.akka.io/api/alpakka-kafka/4.0.2/akka/kafka/ProducerMessage$$Envelope.html#passThrough:PassThrough")
+  (producer-message-with-passthrough [this passthrough] "https://doc.akka.io/api/alpakka-kafka/4.0.2/akka/kafka/ProducerMessage$$Envelope.html#withPassThrough[PassThrough2](value:PassThrough2):akka.kafka.ProducerMessage.Envelope[K,V,PassThrough2]"))
 
 (extend-protocol IProducerMessage
   ProducerMessage$Envelope
@@ -38,39 +41,66 @@
     (.passThrough this)))
 
 (defn flexi-flow
+  "Akka Stream connector for publishing messages to Kafka topics.
+  https://doc.akka.io/api/alpakka-kafka/4.0.2/akka/kafka/javadsl/Producer$.html#flexiFlow[K,V,PassThrough](settings:akka.kafka.ProducerSettings[K,V]):akka.stream.javadsl.Flow[akka.kafka.ProducerMessage.Envelope[K,V,PassThrough],akka.kafka.ProducerMessage.Results[K,V,PassThrough],akka.NotUsed]"
   [^ProducerSettings producer-settings]
   (Producer/flexiFlow producer-settings))
 
 (defn flow-with-context
+  "Akka Stream connector for publishing messages to Kafka topics.
+  https://doc.akka.io/api/alpakka-kafka/4.0.2/akka/kafka/javadsl/Producer$.html#flowWithContext[K,V,C](settings:akka.kafka.ProducerSettings[K,V]):akka.stream.javadsl.FlowWithContext[akka.kafka.ProducerMessage.Envelope[K,V,akka.NotUsed],C,akka.kafka.ProducerMessage.Results[K,V,C],C,akka.NotUsed]"
   [^ProducerSettings producer-settings]
   (Producer/flowWithContext producer-settings))
 
 (defn plain-sink
+  "Akka Stream connector for publishing messages to Kafka topics.
+  https://doc.akka.io/api/alpakka-kafka/4.0.2/akka/kafka/javadsl/Producer$.html#plainSink[K,V](settings:akka.kafka.ProducerSettings[K,V]):akka.stream.javadsl.Sink[org.apache.kafka.clients.producer.ProducerRecord[K,V],java.util.concurrent.CompletionStage[akka.Done]]"
   [^ProducerSettings producer-settings]
   (Producer/plainSink producer-settings))
 
 (defn committable-sink
+  "Akka Stream connector for publishing messages to Kafka topics.
+  https://doc.akka.io/api/alpakka-kafka/4.0.2/akka/kafka/javadsl/Producer$.html#committableSink[K,V,IN%3C:akka.kafka.ProducerMessage.Envelope[K,V,akka.kafka.ConsumerMessage.Committable]](producerSettings:akka.kafka.ProducerSettings[K,V],committerSettings:akka.kafka.CommitterSettings):akka.stream.javadsl.Sink[IN,java.util.concurrent.CompletionStage[akka.Done]]"
   [^ProducerSettings producer-settings ^CommitterSettings committer-settings]
   (Producer/committableSink producer-settings committer-settings))
 
+(defn committable-sink-with-offset-context
+  "Akka Stream connector for publishing messages to Kafka topics.
+  https://doc.akka.io/api/alpakka-kafka/4.0.2/akka/kafka/javadsl/Producer$.html#committableSinkWithOffsetContext[K,V,IN%3C:akka.kafka.ProducerMessage.Envelope[K,V,_],C%3C:akka.kafka.ConsumerMessage.Committable](producerSettings:akka.kafka.ProducerSettings[K,V],committerSettings:akka.kafka.CommitterSettings):akka.stream.javadsl.Sink[akka.japi.Pair[IN,C],java.util.concurrent.CompletionStage[akka.Done]]"
+  [^ProducerSettings producer-settings ^CommitterSettings committer-settings]
+  (Producer/committableSinkWithOffsetContext producer-settings committer-settings))
+
 (defn single-producer-message-envelope
+  "Message publishes a single message to its topic, and continues in the stream as Result
+  https://doc.akka.io/api/alpakka-kafka/4.0.2/akka/kafka/ProducerMessage$$Message.html"
   ^ProducerMessage$Envelope
   [message-offset ^ProducerRecord producer-record]
   (ProducerMessage/single producer-record message-offset))
 
 (defn multi-producer-message-envelope
+  "MultiMessage publishes all messages in its records field, and continues in the stream as MultiResult
+  https://doc.akka.io/api/alpakka-kafka/4.0.2/akka/kafka/ProducerMessage$$MultiMessage.html"
   ^ProducerMessage$Envelope
   [message-offset producer-records]
   (ProducerMessage/multi ^Collection producer-records message-offset))
 
 (defn ->send-producer
+  "Utility Kafka Producer for producing to Kafka without using Akka Streams.
+  https://doc.akka.io/api/alpakka-kafka/4.0.2/akka/kafka/javadsl/SendProducer.html"
   [^ProducerSettings producer-settings ^ActorSystem actor-system]
   (SendProducer. producer-settings actor-system))
 
 (defn publish-via-send-producer
+  "https://doc.akka.io/api/alpakka-kafka/4.0.2/akka/kafka/javadsl/SendProducer.html#send(record:org.apache.kafka.clients.producer.ProducerRecord[K,V]):java.util.concurrent.CompletionStage[org.apache.kafka.clients.producer.RecordMetadata]"
   ^CompletionStage
   [^SendProducer send-producer ^ProducerRecord producer-record]
   (.send send-producer producer-record))
+
+(defn close-send-producer
+  "https://doc.akka.io/api/alpakka-kafka/4.0.2/akka/kafka/javadsl/SendProducer.html#close():java.util.concurrent.CompletionStage[akka.Done]"
+  ^CompletionStage
+  [^SendProducer send-producer]
+  (.close send-producer))
 
 (defn ->producer-record
   "creates a producer record which can be used later for publishing"
