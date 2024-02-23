268ca5de6d878217eddaeea56657cea69a93f1dc
fr33m0nk <prashantsinha@protonmail.com>
2024-02-23 22:36:32 +0530

    [fr33m0nk]: Fix CI

--- build.clj
+++ build.clj
@@ -30,7 +30,7 @@
               :src-dirs ["src"]))
 
 (defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
-      #_(test opts)
+      (test opts)
       (b/delete {:path "target"})
       (let [opts (jar-opts opts)]
            (println "\nWriting pom.xml...")
