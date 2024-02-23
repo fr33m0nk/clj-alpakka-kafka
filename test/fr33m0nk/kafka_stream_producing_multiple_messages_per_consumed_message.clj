61a74a6a478fc1bd0b4f5e678533aa6ea5b776dd
fr33m0nk <prashantsinha@protonmail.com>
2024-02-23 14:24:57 +0530

    [fr33m0nk]: Adds more tests

--- test/fr33m0nk/kafka_stream_producing_multiple_messages_per_consumed_message.clj
+++ test/fr33m0nk/kafka_stream_producing_multiple_messages_per_consumed_message.clj
@@ -1,4 +1,4 @@
-(ns fr33m0nk.kafka-stream-with-produce-test
+(ns fr33m0nk.kafka-stream-producing-multiple-messages-per-consumed-message
   (:require [clojure.string :as str]
             [clojure.test :refer [deftest testing is]]
             [fr33m0nk.akka.actor :as actor]
@@ -11,7 +11,7 @@
   (:import (java.util.concurrent CompletableFuture)
            (org.apache.kafka.common.serialization StringSerializer StringDeserializer)))
 
-(defn kafka-stream-with-producer
+(defn kafka-stream-producing-multiple-messages-per-consumed-message
   [actor-system consumer-settings committer-settings producer-settings consumer-topics producer-topic processing-fn]
   (-> (consumer/->committable-source consumer-settings consumer-topics)
       (s/map-async 2
@@ -19,8 +19,8 @@
                      (let [_key (consumer/key message)
                            value (consumer/value message)
                            committable-offset (consumer/committable-offset message)
-                           message-to-publish (producer/->producer-record producer-topic (processing-fn value))]
-                       (producer/single-producer-message-envelope committable-offset message-to-publish))))
+                           messages-to-publish (processing-fn producer-topic value)]
+                       (producer/multi-producer-message-envelope committable-offset messages-to-publish))))
       (s/to-mat (producer/committable-sink producer-settings committer-settings) consumer/create-draining-control)
       (s/run actor-system)))
 
@@ -37,10 +37,14 @@
             producer-settings (producer/producer-settings actor-system {:bootstrap-servers bootstrap-servers
                                                                         :key-serializer (StringSerializer.)
                                                                         :value-serializer (StringSerializer.)})
-            processing-fn (fn [message] (str/upper-case message))
-            consumer-control (kafka-stream-with-producer actor-system consumer-settings committer-settings producer-settings [in-topic] out-topic processing-fn)
+            processing-fn (fn [producer-topic message]
+                            (->> (repeat 5 message)
+                                 (mapv #(producer/->producer-record producer-topic (str/upper-case %)))))
+            consumer-control (kafka-stream-producing-multiple-messages-per-consumed-message
+                               actor-system consumer-settings committer-settings producer-settings [in-topic] out-topic processing-fn)
             in-messages [{:key "key-1" :value "msg-1"} {:key "key-2" :value "msg-2"} {:key "key-3" :value "msg-3"}]
-            expected-out [{:key nil :value "MSG-1"} {:key nil :value "MSG-2"} {:key nil :value "MSG-3"}]]
+            expected-out (->> [{:key nil :value "MSG-1"} {:key nil :value "MSG-2"} {:key nil :value "MSG-3"}]
+                              (into [] (comp (map #(repeat 5 %)) cat)))]
         (try
           (Thread/sleep 1000)
           (run! (fn [{:keys [key value]}] (ftu/send-record bootstrap-servers in-topic key value)) in-messages)
